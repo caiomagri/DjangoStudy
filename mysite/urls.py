bc31e6428a7dac4946a51b1532873fd33a621050
caio <kaioeduardofm@gmail.com>
2018-03-19 15:05:22 -0300

    fix the bug at media files

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
 from blog import views as blog_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.contrib import admin
 
 urlpatterns = [
     path('post/<slug>',blog_views.post ),
@@ -24,3 +28,5 @@ urlpatterns = [
     path('', blog_views.index ),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
